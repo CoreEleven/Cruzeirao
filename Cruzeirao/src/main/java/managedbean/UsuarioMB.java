7dd03020c45200d4ad2a6efcb881a59544c0ae9e
151002 <151002@A2905.labinfo.facens.br>
2018-05-11 18:24:25 -0300

    Commit inicial

---
+++ Cruzeirao/src/main/java/managedbean/UsuarioMB.java
@@ -0,0 +1,41 @@
+package managedbean;
+
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import modelo.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioMB {
+
+	private ArrayList<Usuario> lista = new ArrayList<>();
+	
+	private Usuario usuario = new Usuario();
+	
+	public void salvar() {
+		lista.add(usuario);
+		usuario = new Usuario();
+	}
+	
+	public ArrayList<Usuario> getLista(){
+		return lista;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setLista(ArrayList<Usuario> lista) {
+		this.lista = lista;
+	}
+	
+	
+	
+}
